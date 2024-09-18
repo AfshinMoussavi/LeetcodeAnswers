efca408ca71af99a5210cff13d53c89f13de1400
Afshin Moussavi <142570526+AfshinMoussavi@users.noreply.github.com>
2024-09-18 18:38:21 +0330

    Create 1929. Concatenation of Array.py

---
+++ 1929. Concatenation of Array.py
@@ -0,0 +1,8 @@
+#  Name   :   Concatenation of Array
+#  URL    :   https://leetcode.com/problems/concatenation-of-array/description/
+#  Github :   https://github.com/AfshinMoussavi
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums+nums
